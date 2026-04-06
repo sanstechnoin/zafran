5e8afa8b06aeb6d8509d0acbec5137214d8301f7
sanstechnoin <65869568+sanstechnoin@users.noreply.github.com>
2026-04-06 22:51:52 +0200

    Update reservation-script.js

--- reservation-script.js
+++ reservation-script.js
@@ -1,18 +1,3 @@
-// --- 1. FIREBASE CONFIGURATION ---
-const firebaseConfig = {
-  apiKey: "AIzaSyAVh2kVIuFcrt8Dg88emuEd9CQlqjJxDrA",
-  authDomain: "zaffran-delight.firebaseapp.com",
-  projectId: "zaffran-delight",
-  storageBucket: "zaffran-delight.firebasestorage.app",
-  messagingSenderId: "1022960860126",
-  appId: "1:1022960860126:web:1e06693dea1d0247a0bb4f"
-};
-
-if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-}
-const db = firebase.firestore();
-
 // --- 2. CONFIGURATION ---
 const RESTAURANT_CAPACITY = 40; 
 const AVERAGE_DURATION_MINUTES = 120; 

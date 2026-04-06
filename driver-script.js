24f080c29b1ae6f856631cf3def1bf84d72a76f0
sanstechnoin <65869568+sanstechnoin@users.noreply.github.com>
2026-04-06 22:49:29 +0200

    Update driver-script.js

--- driver-script.js
+++ driver-script.js
@@ -1,18 +1,4 @@
-// --- 1. CONFIGURATION ---
-const firebaseConfig = {
-  apiKey: "AIzaSyAVh2kVIuFcrt8Dg88emuEd9CQlqjJxDrA",
-  authDomain: "zaffran-delight.firebaseapp.com",
-  projectId: "zaffran-delight",
-  storageBucket: "zaffran-delight.firebasestorage.app",
-  messagingSenderId: "1022960860126",
-  appId: "1:1022960860126:web:1e06693dea1d0247a0bb4f"
-};
-
-// --- 2. INITIALIZE FIREBASE ---
-if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-}
-const db = firebase.firestore();
+// --- 2. INITIALIZE Driver Info from FIREBASE ---
 const driverContainer = document.getElementById('driver-orders');
 
 let watchId = null; 
